70d6a19c27c46e2a251d6dfc92808814965b85a9
collymoore2 <68247899+collymoore2@users.noreply.github.com>
2020-07-15 10:31:53 -0700

    Ejercicio 2
    collymoore c sharp

---
+++ EjerciciosEventos2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjerciciosEventos2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbbColores.SelectedItem == "Azul") {
+                this.BackColor = Color.Blue;
+
+            }
+
+            else if (cbbColores.SelectedItem == "Rojo")
+            {
+                this.BackColor = Color.Red;
+
+            }
+
+            else if (cbbColores.SelectedItem == "Amarillo")
+            {
+                this.BackColor = Color.Yellow;
+
+            }
+
+            else
+            {
+                this.BackColor = Color.Green;
+
+            }
+
+        }
+    }
+}
